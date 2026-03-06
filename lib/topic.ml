fe91c52eac7b0e06f7d9e65baaf7d1180a4abdfa
InnesWarwick <inneswarwick@gmail.com>
2026-03-06 00:36:56 +0000

    rewordwed topics

--- lib/topic.ml
+++ lib/topic.ml
@@ -3,33 +3,21 @@ let generate_person_topic people =
   List.nth people n
 
 let verbs =
-        Futil.read_lines "lib/resources/words/verbs/verbs.txt"
-        |> List.map String.trim
-        |> List.filter (fun s -> String.ends_with ~suffix:"ing" s)
-        
-   
+  Futil.read_lines "lib/resources/words/verbs/verbs.txt"
+  |> List.map String.trim
+  |> List.filter (fun s -> String.ends_with ~suffix:"ing" s)
 
-let adverbs = 
-        Futil.read_lines "lib/resources/words/adverbs/adverbs.txt"
-        |> List.map String.trim
-        |> List.filter (fun s -> String.ends_with ~suffix:"ly" s) 
+let adverbs =
+  Futil.read_lines "lib/resources/words/adverbs/adverbs.txt"
+  |> List.map String.trim
+  |> List.filter (fun s -> String.ends_with ~suffix:"ly" s)
 
 let generate_other_topic () =
-        let nv = Random.int (List.length verbs) in
-        let na = Random.int (List.length adverbs) in
-        let verb = List.nth verbs nv in  
-        let adverb = List.nth adverbs na in
-        adverb ^ " " ^ verb
-        
-let generate_topic people =
-  match Random.int 2 with
-  | 0 ->
-    Futil.info "person";
-    generate_person_topic people
-  | 1 -> 
+  let nv = Random.int (List.length verbs) in
+  let na = Random.int (List.length adverbs) in
+  let verb = List.nth verbs nv in
+  let adverb = List.nth adverbs na in
+  adverb ^ " " ^ verb
 
-    Futil.info "other";
-                  generate_other_topic ()
-  | _ ->
-      Futil.fatal "Unreachable path in Topic.generate_topic";
-      raise (Failure "see logs for more details")
+let generate_topic people =
+  Futil.rand_from_list [ generate_person_topic people; generate_other_topic () ]
